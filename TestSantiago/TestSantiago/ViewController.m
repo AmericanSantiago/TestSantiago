738957ba242f04426f5823ec38afbbca44832d18
李祖建 <844142553@qq.com>
2016-06-21 00:22:50 +0800

    我在ViewController写了个NSLog

--- TestSantiago/TestSantiago/ViewController.m
+++ TestSantiago/TestSantiago/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"美国上帝压狗~");
 }
 
 - (void)didReceiveMemoryWarning {
